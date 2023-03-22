f32b7a51e9b741bd466441fc62443ad779e87bf9
Montanah <mititi.isaac@gmail.com>
2023-03-22 15:16:27 -0700

    Review.py update

--- models/review.py
+++ models/review.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'reviews'
+        user_id = Column(String(60),
+                         ForeignKey('users.id', ondelete='cascade'),
+                         nullable=False)
+        place_id = Column(String(60),
+                          ForeignKey('places.id', ondelete='cascade'),
+                          nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
