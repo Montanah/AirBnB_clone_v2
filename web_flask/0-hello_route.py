a509db9b83b200180412165b6a247cc4e0fcb0c4
Montanah <mititi.isaac.com>
2023-06-16 02:33:06 -0700

    Editing app for testing

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,19 +3,19 @@
 a script that starts a Flask web app
 '''
 
-from flask import Flask
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     '''
         tests basic flask app
     '''
-    return "Hello HBNB!"
+    return render_template("5-number.html")
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
